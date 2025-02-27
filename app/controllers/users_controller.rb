f637b271211deeab4eeb310a9aa6f56be6e6b987
sii-yan <naobeedesign@gmail.com>
2025-02-27 13:53:10 +0900

    feat: deviseのview(ログイン、新規登録ページ)の作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "アカウント情報を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :profile_image, :bio)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
-  get 'rooms/new'
-  get 'rooms/create'
-  get 'rooms/edit'
-  get 'rooms/update'
-  get 'rooms/destory'
-  root 'home#index'
-  devise_for :users
 
+  root 'home#index'
+
+  devise_for :users
   resources :rooms
   resources :reservations
 end
