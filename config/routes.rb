811fad0c467c47d9157773da411847cf11d79fb1
sii-yan <naobeedesign@gmail.com>
2025-02-27 17:48:41 +0900

    feat: 各scssファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
+
+  resources :users, only: [:show, :edit, :update] # ユーザー詳細・編集
   resources :rooms
   resources :reservations
 end
